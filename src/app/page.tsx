120fcc2fd5c8816daf4577a6fef13c533c244562
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2024-11-09 10:27:05 -0300

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { NavBar } from '@/components/nav-bar'
+
 export default function Home() {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold text-center">
-        Welcome to the largest dealer in America
-      </h1>
-    </div>
+    <main className="w-full h-screen flex justify-center relative">
+      <NavBar />
+      <section className="w-full h-full max-w-7xl flex flex-col items-center justify-center p-4">
+        <h1 className="text-5xl font-bold text-center">
+          Welcome to the{' '}
+          <span className="bg-gradient-to-r from-pink-700 to-violet-600 text-transparent bg-clip-text">
+            largest dealer
+          </span>{' '}
+          in America
+        </h1>
+      </section>
+    </main>
   )
 }
